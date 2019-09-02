3f339c0f9eebd0cd16a4b78a1a446a63f4f7c460
Tyler Kennedy <tke34@uclive.ac.nz>
2019-09-02 20:46:13 +1200

    Improve templating
    * add more sample rules

--- frontend/src/components/fragments/Rules.vue
+++ frontend/src/components/fragments/Rules.vue
@@ -10,11 +10,19 @@
       />
     </b-form-group>
     <b-card-group v-if="rules.length">
-      <b-card v-for="rule in rules" v-bind:key="rule.id" class="card">
-        MAKE ME PRETTY
-        {{rule.name}}
-        {{rule.desc}}
-        {{rule.points}}
+      <b-card
+        v-for="rule in rules"
+        v-bind:key="rule.id"
+        class="card"
+        :title="rule.name"
+        bg-variant="dark"
+        text-variant="white"
+        align="left"
+      >
+        <b-row>
+          <b-col>{{rule.desc}}</b-col>
+          <b-col sm="1">{{rule.points}}</b-col>
+        </b-row>
       </b-card>
     </b-card-group>
   </div>
@@ -36,25 +44,28 @@ export default {
       this.$http
         .get(this.url + "/games")
         .then(function(response) {
-          console.log(response.body)
-          this.games = response.body
+          console.log(response.body);
+          this.games = response.body;
         })
         .catch(function(reason) {
-          console.log(reason)
-          this.games = []
+          console.log(reason);
+          this.games = [];
         });
     },
     getRulesForGame: function(game) {
-      this.$http.get(this.url + "/games/" + game + "/rules").then( function(response) {
-        this.rules = response.body
-      }).catch(function(reason) {
-        console.log(reason)
-        this.rules = []
-      })
+      this.$http
+        .get(this.url + "/games/" + game + "/rules")
+        .then(function(response) {
+          this.rules = response.body;
+        })
+        .catch(function(reason) {
+          console.log(reason);
+          this.rules = [];
+        });
     }
   },
   mounted: function() {
-    this.getGames()
+    this.getGames();
   }
 };
 </script>
--- frontend/src/components/fragments/Scores.vue
+++ frontend/src/components/fragments/Scores.vue
@@ -1,33 +1,53 @@
 <template>
   <b-card-group>
-    <b-card v-for="score in scores" v-bind:key="score.user_id" class="card">
-        MAKE ME PRETTY
-        {{score.username}}
-        {{score.score}}
+    <b-card
+      v-for="score in scores"
+      v-bind:key="score.user_id"
+      class="card"
+      bg-variant="dark"
+      text-variant="white"
+      align="left"
+    >
+      <b-row>
+        <b-col>{{score.username}}</b-col>
+        <b-col sm="2">{{score.score}}</b-col>
+      </b-row>
     </b-card>
-</b-card-group>  
+  </b-card-group>
 </template>
 
 <script>
 export default {
-    name: 'Scores',
-    data () {
-        return {
-            scores: [{
-                'user_id': 1,
-                'username': 'Number Juan Champ',
-                'score': 42
-            }],
+  name: "Scores",
+  data() {
+    return {
+      scores: [
+        {
+          user_id: 1,
+          username: "Number One Champ",
+          score: 142
+        },
+        {
+            user_id: 2,
+            username: "Nubuscu",
+            score: -3
+        },
+        {
+            user_id: 3,
+            username: "sbeve",
+            score: 1
         }
-    },
-    mounted: function () {
-        // TODO polling? websockets/pusher?
-    }
-}
+      ]
+    };
+  },
+  mounted: function() {
+    // TODO polling? websockets/pusher?
+  }
+};
 </script>
 
 <style>
 .card {
-    min-width: 100%;
+  min-width: 100%;
 }
 </style>
