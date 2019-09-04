3d36a8dbd2701ae4103f4c131408888becb6245d
Tyler Kennedy <tke34@uclive.ac.nz>
2019-09-04 17:35:46 +1200

    Stop routes from being created with null db object

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('./app/config/express')
 const PORT = process.env.PORT || 5000
-const app = express()
 const { connectToMongoDb } = require('./app/config/db.js');
 
 connectToMongoDb()
     .then(() => {
         console.log('MongoDB: Successfully connected');
+        const app = express()
         app.listen(PORT, () => {
             console.log('listening on port: ' + PORT)
         })
